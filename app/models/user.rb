e55b6841cbbad10d22e0e324a0236b666f04590b
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-29 22:03:33 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #passwordは英数混合で6文字以上
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
+  with_options presence: true do
+    validates :nickname
+    validates :name,         format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :name_kana,    format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :age,          format: { with: /\A[0-9]+\z/ }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  end
+
 end
