2496c84a84e3f90e103f0ef02ce5112087645064
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-30 00:31:47 +0900

    ageのバリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   #passwordは英数混合で6文字以上
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
   with_options presence: true do
-    validates :nickname
+    validates :nickname,     length: { minimum: 1, maximum: 40 }
     validates :name,         format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :name_kana,    format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :age,          format: { with: /\A[0-9]+\z/ }
+    validates :age,          numericality: { greater_than_or_equal_to: 20 }
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
 
