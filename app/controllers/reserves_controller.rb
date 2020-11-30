43e317f962a6bb6e2e012b6610e5c2a06c977265
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-30 14:31:26 +0900

    reserveコントローラー

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -1,3 +1,24 @@
 class ReservesController < ApplicationController
   def index
+  end
+
+  def new
+    @reserve = Reserve.new
+  end
+
+  def create
+    @reserve = Reserve.new(reserve_params)
+    if @reserve.valid?
+      @reserve.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+
+  private
+  def reserve_params
+    params.require(:reserve).permit(:reserve_date, :reserve_time, :number_reserve, :reserve_category_id).merge(user_id: current_user.id)
+  end
 end
