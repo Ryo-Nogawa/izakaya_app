b416800dd1f6a8e155135e3720bb3855d33c0f05
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-12-01 07:14:31 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'reserves#index'
   resources :reserves, only: [:new, :create]
   resources :visuals, only: [:index, :new, :create, :edit, :update, :destroy, :show]
+  resources :chats, only: [:index, :new]
 end
