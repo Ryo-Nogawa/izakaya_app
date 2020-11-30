b2d146b13167a71944e51324565cb19cf37738bf
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-30 20:56:18 +0900

    ブログ機能実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,40 @@
 class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.valid?
+      @blog.save
+      redirect_to blogs_path
+    else
+      redirect_to new_blog_path
+    end
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    if blog.valid?
+      blog.destroy
+      redirect_to blogs_path
+    end
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :text, images: []).merge(user_id: current_user.id)
+  end
 end
