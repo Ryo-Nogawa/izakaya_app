86c11f503e0b124b3d229cd9944682bc81034f43
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-30 17:38:45 +0900

    indexアクション

--- app/controllers/visuals_controller.rb
+++ app/controllers/visuals_controller.rb
@@ -1,6 +1,40 @@
 class VisualsController < ApplicationController
 
   def index
-    @visuals = Visuals.all
+    @visuals = Visual.all
+  end
+
+  def new
+    @visual = Visual.new
+  end
+
+  def create
+    visual = Visual.new(visual_params)
+    if visual.valid?
+      visual.save
+      redirect_to visuals_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @visual = Visual.find(params[:id])
+  end
+
+  def 
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def visual_params
+    params.require(:visual).permit(:image).merge(user_id: current_user.id)
   end
 end
