33433dc4573e2cef11e49288dbc1f94f0be1409a
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-30 14:29:06 +0900

    visualモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'reserves#index'
+  resources :reserves, only: [:new, :create]
+  resources :visuals, only: [:index, :new, :create, :edit, :update, :destroy]
 end
