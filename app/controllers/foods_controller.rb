c308b9ada5bdc503921e9d250276269ee16680cc
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-12-01 20:19:59 +0900

    新規作成ページ

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,25 @@
 class FoodsController < ApplicationController
+
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.valid?
+      @food.save
+      redirect_to foods_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:title, :detail, :price, :image, :food_category_id).merge(user_id: current_user.id)
+  end
 end
