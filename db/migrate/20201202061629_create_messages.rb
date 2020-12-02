e0d853ece1be20245b4eea2afced561a0a9f1e4b
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-12-02 15:21:44 +0900

    メッセージ機能

--- db/migrate/20201202061629_create_messages.rb
+++ db/migrate/20201202061629_create_messages.rb
@@ -1,8 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
+      t.references :user,    foregin_key: true
       t.string     :content, null: false
-      t.references :user,    foreign_key: true
+
       t.timestamps
     end
   end
