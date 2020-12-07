5668c36f5c6dcb058419ee536b06b3101da4962a
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-12-07 16:16:36 +0900

    drinkのバリデーション

--- app/models/drink_comment.rb
+++ app/models/drink_comment.rb
@@ -1,4 +1,7 @@
 class DrinkComment < ApplicationRecord
   belongs_to :user
   belongs_to :drink
+
+  コメントの最大文字数は250文字
+  validates :comment, presence: true, length: { minimum: 1, maximum: 250 }
 end
