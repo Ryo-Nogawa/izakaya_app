07b5378fd1cec420a8bb9ffc58a0dfc38eab31b1
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-12-07 19:39:00 +0900

    visualのエラーメッセージの日本語化

--- app/models/visual.rb
+++ app/models/visual.rb
@@ -2,6 +2,6 @@ class Visual < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  # 画像は必須
-  validates :image, presence: true
+    # 画像は必須
+    validates :image, presence: true
 end
