c7c0d9456a0869cbcd5dc5a07d2150d71138bb28
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-12-07 16:08:36 +0900

    chatのバリデーション

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,7 @@
 class Chat < ApplicationRecord
   has_many :chat_users
   has_many :users, through: :chat_users
+
+  チャットルームの名前は最大40文字
+  validates :name, presence: true, length: { minimum: 1, maximum: 40 }
 end
