857447b50bce29248c12e45d6d9f469407955d96
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-12-18 17:58:49 +0900

    予約入力画面のレイアウト

--- app/models/reserve_category.rb
+++ app/models/reserve_category.rb
@@ -1,6 +1,6 @@
 class ReserveCategory < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--------' },
+    { id: 1, name: '選択してください' },
     { id: 2, name: 'お席のみ' },
     { id: 3, name: '飲み放題' },
     { id: 4, name: '食べ飲み放題' }
