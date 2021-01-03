c9f89014f2d3589df91acb4b925264ad28c13013
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2021-01-03 19:27:20 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :edit_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
 
@@ -11,5 +12,11 @@ class ApplicationController < ActionController::Base
 
   def edit_permitted_parameters
     devise_parameter_sanitizer.permit(:edit, keys: [:nickname, :name, :name_kana, :age, :phone_number])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
