5a4be20c4f02328803605d69418af5921d3c5391
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2021-01-10 00:24:02 +0900

    bookモデルのテストコード

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :book do
-    
+    reserve_date        {Faker::Date.between(from: 1.day.from_now, to: 1.year.from_now)}
+    reserve_time        {"17:00"}
+    number_reserve      {Faker::Number.between(from: 2, to: 20)}
+    reserve_category_id {Faker::Number.between(from: 2, to: 4)}
+    association :user
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @book = FactoryBot.build(:book)
+  end
+
+  describe '予約する' do
+    context '予約が完了する時' do
+      it 'reserve_date, reserve_time, number_reserve, reserve_category_idが存在する時' do
+        expect(@book).to be_valid
+      end
+    end
+
+    context '予約が失敗する時' do
+      it 'reserve_dateが過去の日付' do
+        @book.reserve_date = '2020-12-30'
+        @book.valid?
+        expect(@book.errors.full_messages).to include('御予約日時は明日以降の日付を選択してください')
+      end
+      it 'reserve_timeが空' do
+        @book.reserve_time = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include('御予約時間を入力してください')
+      end
+      it 'number_reserveが1' do
+        @book.number_reserve = 1
+        @book.valid?
+        expect(@book.errors.full_messages).to include('御予約人数は1より大きい値にしてください')
+      end
+      it 'number_reserveが21以上' do
+        @book.number_reserve = Faker::Number.between(from: 21, to: 100)
+        @book.valid?
+        expect(@book.errors.full_messages).to include('御予約人数は21より小さい値にしてください')
+      end
+      it 'reserve_category_idが1' do
+        @book.reserve_category_id = 1
+        @book.valid?
+        expect(@book.errors.full_messages).to include('御予約内容は1以外の値にしてください')
+      end
+    end
+  end
 end
