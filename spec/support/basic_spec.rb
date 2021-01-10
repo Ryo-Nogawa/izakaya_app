b9b16cf8b32316995f6d992666ac27c9d2e7a91f
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2021-01-11 02:35:53 +0900

    新規登録がうまくいかない場合の結合テストコード

--- spec/support/basic_spec.rb
+++
@@ -1,7 +0,0 @@
-module BasicAuth
-  def basic_auth(path)
-    username = ENV["BASIC_AUTH_USER"]
-    password = ENV["BASIC_AUTH_PASSWORD"]
-    visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
-  end
-end
