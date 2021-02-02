e810d41147f5d5df2b0c51979f77f9be14eee92f
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2021-02-02 18:44:09 +0900

    コメントの結合テストコード

--- spec/system/food_spec.rb
+++ spec/system/food_spec.rb
@@ -277,4 +277,54 @@ RSpec.describe 'フード', type: :system do
       expect(page).to have_no_content("削除")
     end
   end
+
+  context "フードメニューにコメントを残す" do
+    it "入力内容があればコメントを残すことができる" do
+      # adminでログインする
+      visit new_user_session_path
+      fill_in "user[email]", with: "admin@admin.com"
+      fill_in "user[password]", with: "000aaa"
+      find('input[name="commit"]').click 
+      expect(current_path).to eq root_path
+      # お料理ボタンをクリックする
+      expect(page).to have_content("お料理")
+      visit foods_path
+      # 新規作成ボタンをクリックする
+      find_link("新規作成", href: new_food_path).click 
+      expect(current_path).to eq new_food_path
+      # フォームを入力する
+      fill_in "food[title]", with: @food.title
+      fill_in "food[detail]", with: @food.detail
+      fill_in "food[price]", with: @food.price
+      select "今月のおすすめ", from: "food[food_category_id]"
+      attach_file("food[image]", "public/images/test_image.png", make_visible: true)
+      # 投稿するボタンをクリック
+      expect do
+        find('input[value="投稿する"]').click
+      end.to change { Food.count }.by(1)
+      # 投稿完了ページに遷移することを確認する
+      expect(page).to have_content("投稿が完了しました")
+      # フード一覧ページに投稿した内容が存在する
+      find_link("一覧へ戻る", href: foods_path).click 
+      expect(page).to have_selector("img[src$='test_image.png']")
+      expect(page).to have_content(@food.title)
+      expect(page).to have_content(@food.price)
+      expect(page).to have_content("今月のおすすめ")
+      # ログアウトする
+      click_on "ログアウト"
+      # ユーザー新規登録する
+      user_regitstration(@user)
+      # ログインする
+      sign_in(@user)
+      # フード一覧ページへ遷移する
+      expect(page).to have_content("お料理")
+      visit foods_path
+      # メニューの画像をクリックする
+      find(".index-content-image").click 
+      # コメント欄にコメント内容を入力する
+      fill_in "food_comment[comment]", with: Faker::Lorem.characters(number: 100)
+      # 送信ボタンをクリックする
+      find(".comment-submit").click 
+    end
+  end
 end
