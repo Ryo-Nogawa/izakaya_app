c2f12cd4fedc90c7323934d05d5cafcbd9b7b5ff
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2021-02-03 19:15:37 +0900

    削除ができない時の結合テストコード

--- spec/system/blog_spec.rb
+++ spec/system/blog_spec.rb
@@ -190,4 +190,46 @@ RSpec.describe 'ブログ', type: :system do
       expect(page).to have_no_content(@blog.title)
     end
   end
+
+  context "削除ができない時" do
+    it "admin以外でログインしているときは削除できない" do
+      # adminでログインする
+      visit new_user_session_path
+      fill_in "user[email]", with: "admin@admin.com"
+      fill_in "user[password]", with: "000aaa"
+      find('input[name="commit"]').click 
+      expect(current_path).to eq root_path
+      # ブログボタンをクリック
+      expect(page).to have_content("ブログ")
+      visit blogs_path
+      # 新規投稿ボタンをクリック
+      find_link("新規投稿", href: new_blog_path).click 
+      expect(current_path).to eq new_blog_path
+      # フォームを入力する
+      attach_file("blog[image]", "public/images/test_image.png", make_visible: true)
+      fill_in "blog[title]", with: @blog.title
+      fill_in "blog[text]", with: @blog.text
+      # 投稿するボタンをクリックする
+      expect do
+        find('input[value="投稿する"]').click
+      end.to change { Blog.count }.by(1)
+      # 投稿完了ページに遷移することを確認する
+      expect(page).to have_content("投稿が完了しました")
+      # ブログ一覧ページに投稿した内容が存在する
+      find_link("一覧へ戻る", href: blogs_path).click 
+      expect(page).to have_selector("img[src$='test_image.png']")
+      expect(page).to have_content(@blog.title)
+      # ログアウトする
+      click_on "ログアウト"
+      # ユーザー新規登録
+      user_regitstration(@user)
+      # ログインする
+      sign_in(@user)
+      # ブログ一覧ページへ遷移する
+      expect(page).to have_content("ブログ")
+      visit blogs_path
+      # 削除ボタンがないことを確認する
+      expect(page).to have_no_content("削除")
+    end
+  end
 end
