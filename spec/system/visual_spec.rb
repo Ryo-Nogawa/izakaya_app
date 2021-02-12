86d71ea71c678b6a8606aefa785706abb00adb95
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2021-02-12 16:36:01 +0900

    削除ができない時の結合テストコード

--- spec/system/visual_spec.rb
+++ spec/system/visual_spec.rb
@@ -174,5 +174,44 @@ RSpec.describe 'フード', type: :system do
       find_link("一覧へ戻る", href: visuals_path).click
       expect(page).to have_no_selector("img[src$='test_image.png']")
     end
+
+    it "admin以外でログインしている時は削除できない" do
+      # adminでログインする
+      visit new_user_session_path
+      fill_in "user[email]", with: "admin@admin.com"
+      fill_in "user[password]", with: "000aaa"
+      find('input[name="commit"]').click
+      # 外観/内装ボタンをクリック
+      expect(page).to have_content("外観/内装")
+      visit visuals_path
+      # 新規作成ボタンをクリック
+      find_link("新規作成", href: new_visual_path).click
+      expect(current_path).to eq new_visual_path
+      # フォームを入力する
+      attach_file("visual[image]", "public/images/test_image.png", make_visible: true)
+      select "外観", from: "visual[visual_category_id]"
+      # 投稿するボタンをクリック
+      expect do
+        find('input[value="投稿する"]').click
+      end.to change { Visual.count }.by(1)
+      # 投稿完了ページに遷移することを確認する
+      expect(page).to have_content("投稿が完了しました")
+      # 外観/内装一覧ページに投稿した内容が存在する
+      find_link("一覧へ戻る", href: visuals_path).click
+      expect(page).to have_selector("img[src$='test_image.png']")
+      # 削除ボタンがあることを確認する
+      expect(page).to have_content("削除")
+      # ログアウトする
+      click_on "ログアウト"
+      # ユーザー新規登録する
+      user_regitstration(@user)
+      # ログインする
+      sign_in(@user)
+      # 外装/内装一覧ページへ遷移する
+      expect(page).to have_content("外観/内装")
+      visit visuals_path
+      # 削除ボタンがないことを確認する
+      expect(page).to have_no_content("削除")
+    end
   end
 end
